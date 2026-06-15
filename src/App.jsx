22eb8195ccbb4fb39f1082af85af52188171c69c
Varshana24 <varshanamp23@gmail.com>
2026-06-15 11:39:25 +0400

    Modified Title

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
     return (
-      <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <div className="min-h-screen flex justify-center bg-gray-900">
         <h1 className="text-5xl font-bold text-white">
           Silent Signal
         </h1>
