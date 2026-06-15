50522273f6f756555f0abbc71257c4d2abd71f12
Varshana24 <varshanamp23@gmail.com>
2026-06-15 11:19:35 +0400

    Inital Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-gray-900">
+        <h1 className="text-5xl font-bold text-white">
+          Silent Signal
+        </h1>
+      </div>
+    );
+  }
+  
+  export default App;
